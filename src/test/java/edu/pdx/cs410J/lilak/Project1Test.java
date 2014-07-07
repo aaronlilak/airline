cc61eee9422e1a4c882e5bd78d5e9f19e809fdb2
Aaron <lilak@pdx.edu>
2014-07-06 22:34:58 -0700

    changed the unconfigured airline message

---
+++ src/test/java/edu/pdx/cs410J/lilak/Project1Test.java
@@ -0,0 +1,59 @@
+package edu.pdx.cs410J.lilak;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import edu.pdx.cs410J.InvokeMainTestCase;
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Tests the functionality in the {@link Project1} main class.
+ */
+public class Project1Test extends InvokeMainTestCase {
+
+    /**
+     * Invokes the main method of {@link Project1} with the given arguments.
+     */
+    private MainMethodResult invokeMain(String... args) {
+        return invokeMain( Project1.class, args );
+    }
+
+  /**
+   * Tests that invoking the main method with no arguments issues an error
+   */
+  @Test
+  public void testNoCommandLineArgumentsError() {
+    MainMethodResult result = invokeMain();
+    assertEquals(new Integer(1), result.getExitCode());
+  }
+
+  @Test
+  public void testFiveCommandLineArgumentsError() {
+    MainMethodResult result = invokeMain("CS410Airline", "1503", "PDX", "20:07 7/5/2014", "MDW");
+    assertEquals(new Integer(1), result.getExitCode());
+  }
+
+  @Test
+  public void testSixCommandLineArgumentsGood() {
+    MainMethodResult result = invokeMain("CS410Airline", "1503", "PDX", "20:07 7/5/2014", "MDW", "23:38 7/5/2014");
+    assertEquals(new Integer(0), result.getExitCode());
+  }
+
+  @Test
+   public void testFiveCommandLineArgumentsWithReadme() {
+     MainMethodResult result = invokeMain("-README","CS410Airline", "1503", "PDX", "20:07 7/5/2014", "MDW");
+     assertEquals(new Integer(0), result.getExitCode());
+   }
+
+  @Test
+  public void testReadmeWithNoArgsIsOK() {
+    MainMethodResult result = invokeMain("-README");
+      assertEquals(new Integer(0), result.getExitCode());
+  }
+
+  @Test
+  public void testReadmeWithSomeArgsIsOK() {
+      MainMethodResult result = invokeMain("CS410Airline", "-README");
+      assertEquals(new Integer(0), result.getExitCode());
+  }
+
+}
